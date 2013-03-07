4f6d85df797d6464fad6bc418d3675fc51339d1e
tomtomtomtom <toms@snailgamesusa.com>
2013-03-07 15:03:00 -0800

    commit fo 3/7/2013 make option page prettier and some jquery that should go with it

--- js/options.js
+++ js/options.js
@@ -6,6 +6,8 @@ var rtCal = 0;
 var woMacro = {protein:0, carbs:0, fat:0};
 var rtMacro = {protein:0, carbs:0, fat:0};
 var personalData = {gender:'m', height_ft:0, height_in:0, weight_lb:0, age:0, activity:0};
+var bmr, tdee, twee, week_cal, week_diff;
+var z = 0;
 
 function load_options() {
 	chrome.storage.local.get('lg', function(items) {
@@ -31,7 +33,7 @@ function load_options() {
 			restore_options();
 		} else {
 			alert('Please enter your Leangains info');
-			$('#workout_m,#rest_t,#workout_w,#rest_th,#workout_f,#rest_sa,#rest_su').attr('checked',true);
+			$('#workout_m,#rest_t,#workout_w,#rest_th,#workout_f,#rest_sa,#rest_su,#gender_m').attr('checked',"checked");
 		}
 	});
 
@@ -60,23 +62,23 @@ function save_options() {
 	var rtmac = {protein:$('#rt-protein').val(), carbs:$('#rt-carbs').val(), fat:$('#rt-fat').val()}
 	// console.log(womac,rtmac);
 
-	// var gender = $('[name=gender]:checked').val();
-	// console.log('gender: ',gender);
+	var gender = $('[name=gender]:checked').val();
+	console.log('gender: ',gender);
 
-	// var hf = $('#height_ft').val();
-	// var hi = $('#height_in').val();
-	// console.log(hf,hi);
+	var hf = $('#height_ft').val();
+	var hi = $('#height_in').val();
+	console.log(hf,hi);
 
-	// var wl = $('#weight_lb').val();
-	// console.log(wl);
+	var wl = $('#weight_lb').val();
+	console.log(wl);
 
-	// var ag = $('#age').val();
-	// console.log(ag);
+	var ag = $('#age').val();
+	console.log(ag);
 
-	// var ac = $('#activity').val();
-	// console.log(ac);
+	var ac = $('#activity').val();
+	console.log(ac);
 
-	var theValue = {woDays:wods, rtds:rtds, woCal:woc, rtCal:rtc, woMacro:womac, rtMacro:rtmac};//, gender:gender, ht:{foot:hf, inch:hi}, weight:wl, age:ag, activity:ac};
+	var theValue = {woDays:wods, rtds:rtds, woCal:woc, rtCal:rtc, woMacro:womac, rtMacro:rtmac, gender:gender, ht:{foot:hf, inch:hi}, weight:wl, age:ag, activity:ac};
 	chrome.storage.local.set({'lg': theValue}, function() {
 		// Notify that we saved.
 		console.log('Settings saved');
@@ -122,16 +124,79 @@ function restore_options() {
 
 	// load personal data
 	// console.log(lg.activity);
-	// $('#gender_'+lg.gender).attr('checked','checked');
-	// $('#height_ft').val(lg.ht.foot);
-	// $('#height_in').val(lg.ht.inch);
-	// $('#height_cm').val((parseFloat(lg.ht.foot*12)+parseFloat(lg.ht.inch))*2.54);
-	// $('#weight_lb').val(lg.weight);
-	// $('#weight_kg').val(parseFloat(lg.weight/2.2).toFixed(2));
-	// $('#age').val(lg.age);
-	// $('#activity').val(lg.activity);
+	$('#gender_'+lg.gender).attr('checked','checked');
+	$('#height_ft').val(lg.ht.foot);
+	$('#height_in').val(lg.ht.inch);
+	$('#height_cm').val((parseFloat(lg.ht.foot*12)+parseFloat(lg.ht.inch))*2.54);
+	$('#weight_lb').val(lg.weight);
+	$('#weight_kg').val(parseFloat(lg.weight/2.2).toFixed(2));
+	$('#age').val(lg.age);
+	$('#activity').val(lg.activity);
 
-	// Calculate TDEE
+	// Calculate TDEE and all that good stuff
+	do_the_bmr();
+	check_input();
+}
+
+function do_the_bmr() {
+	var bmr_ar = [];
+	var bmr_gender = $('[name=gender]:checked').val();
+	$('.watched').each(function(ind, el){
+		//console.log(ind, el, $(el).val());
+		bmr_ar[$(el).attr('id')] = $(el).val();
+	});
+	console.log(bmr_ar);
+	var cal_type = (bmr_ar.body_fat.length == 0) ? 1 : 0; 
+	var cal_variable = (bmr_gender == 'm') ? 5 : -161;
+	bmr = Math.floor( (10*bmr_ar.weight_kg)+(6.25*bmr_ar.height_cm)-(5*bmr_ar.age)+(cal_variable) );
+	$('#bmr').val( bmr );
+	do_the_tdee();
+}
+
+function do_the_tdee() {
+	var activity_level = {0:1.2, 1:1.375, 2:1.55, 3:1.725, 4:1.9};
+	var activity_multiplier = activity_level[$('#activity').val()];
+	console.log(bmr,activity_multiplier);
+	tdee = Math.floor(bmr*activity_multiplier);
+	$('#tdee').val(tdee);
+	do_the_twee();
+}
+
+function do_the_twee() {
+	twee = tdee * 7;
+	$('#twee').val(twee);
+	do_the_week_cal();
+}
+
+function do_the_week_cal() {
+	week_cal = parseInt($('#wo-cal-input').val())*3 + parseInt($('#rt-cal-input').val())*4;
+	$('.week_cal').text(week_cal);
+	do_the_week_diff();
+	add_pos_neg('.week_cal', week_cal);
+}
+
+function do_the_week_diff() {
+	week_diff = week_cal-twee;
+	$('.week_diff').text(week_diff);
+	do_the_week_change();
+	add_pos_neg('.week_diff', week_diff);
+}
+
+function do_the_week_change() {
+	var week_change = parseFloat(week_diff/3500);
+	$('.week_change').text(week_change);
+	add_pos_neg('.week_change', week_change);
+	if (z++ < 1) $('#bmr,#tdee,#twee').parent().wrap('<div class="control-group info"></div>');
+}
+
+function add_pos_neg(ele_name, value) {
+	var the_class = (value > 0) ? 'positive' : 'negative';
+	$(ele_name).addClass(the_class);
+}
+
+function check_input() {
+	console.log('check_input');
+	$('.control-group').find('input[value!=""]').end().addClass('success');
 }
 
 $(document).ready(function() {
@@ -163,4 +228,19 @@ $(document).ready(function() {
 		console.log('delete');
 	});
 
+	$('#height_ft,#height_in').focusout(function(){
+		if ($(this).val()) {
+			$('#height_cm').val( (parseFloat($('#height_ft').val()*12) + parseFloat($('#height_in').val())) *2.54);
+		}
+	});
+
+	$('#weight_lb').focusout(function(){
+		if ($(this).val()) {
+			$('#weight_kg').val(parseFloat($('#weight_lb').val()/2.2).toFixed(2));
+		}
+	});
+	
+	$('.watched').focusout(function(){do_the_bmr()});
+	$('[name=gender]').change(function(){do_the_bmr()});
+	$('input').focusout(function(){console.log('yay');check_input();});
 });
